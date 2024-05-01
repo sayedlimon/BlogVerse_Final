0267b49a0989187e5ab77b1cd18b4265b8c8c980
Md. Mountasir Rahaman <141022092+mounsiam@users.noreply.github.com>
2024-05-01 23:27:45 +0600

    Delete context_processors.py

--- context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Category
-
-
-def get_all_categories(request):
-    categories = Category.objects.all()
-    context = {
-        "categories": categories
-    }
-    return context
